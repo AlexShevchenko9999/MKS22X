a95f5b56e94f9d8dea18dcd7192922ab59d13357
Alex Shevchenko <alexshevchenko9999@gmail.com>
2017-03-15 13:54:52 -0400

    ADd

--- 06/Quiz2Redux.java
+++ 06/Quiz2Redux.java
@@ -1,1 +1,31 @@
+import java.util.*;
+public class Quiz2Redux{  
+    /*Returns an ArrayList<String> that contains all subsets of the
+     *characters of String s. Assume s has no duplicate characters.
+     *the characters should appear in the same order that they occur 
+     *in the original string.
+     */
+    public static ArrayList<String> combinations(String s){
+	ArrayList<String>words = new ArrayList<String>();
+	help( words,"",s ,0);
+	Collections.sort(words);
+	return words;
+    }
+  
+    private static void help( ArrayList<String> words,String newStr,  String s, int counter ){
+	/*METHOD TO BE WRITTEN BY YOU.*/
+	if (s.length() == counter){
+	    words.add(newStr);
+	    return;
+	}
+	help(words, newStr, s, counter+1);
+	help(words, s.substring(counter,counter+1) + newStr, s, counter + 1);
+	
+    }
 
+
+    public static void main(String [] args){
+	System.out.println(combinations("abc"));
+    }
+
+}
