c39aca2a93007d3460c9afa2b3839fb1ec98c1f1
Alex Shevchenko <alexshevchenko9999@gmail.com>
2017-03-21 22:53:48 -0400

    Finished quicksort:it was just calling partition once witch recurses.

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,59 +1,81 @@
 import java.util.*;
 public class Quick{
 
-    
-
-    
-    
     //==================================================================================================
 
     public static int quickselect(int []data, int k){
 	//return the value that is the kth smallest value of the array. 
 	//use your partition method to help you accomplish this.
-	int var = partition(data, 0, data.length - 1);
+	int var = part(data, 0, data.length - 1);
 	while (k != var){
 	    System.out.println("Var:" + var);
 	    System.out.println(arr(data));
-	    if (var > k) var = partition(data, 0, var);
-	    if (var < k) var = partition(data, var+1,data.length-1);
+	    if (var > k) var = part(data, 0, var);
+	    if (var < k) var = part(data, var+1,data.length-1);
 	}
 	return data[k];
     }
 
+    private static int part(int []data, int start, int end){
+        Random r = new Random();
+        int pivot = r.nextInt(end-start) + start;
+        int v = data[pivot];
+        int i = start+1;
+        System.out.println("Pivot: " + pivot + "\n V: " + v + "\n");
+        swap(data, pivot, start);
+        //System.out.println(arr(data));                                                                                                                                             
+        while (i<=end){
+            if (data[i]==v) i++;
+            else if (data[i]<v){
+                swap(data,start,i);
+                i++;
+                start++;
+            }
+            else {
+                swap(data,end,i);
+                end--;
+            }
+            //System.out.println(arr(data));                                                                                                                                         
+        }
+        //System.out.println(arr(data));                                                                                                                                             
+	return (start + end)/2;
+    }
+
+
     //===================================================================================================
 
-    public static int partition(int []data, int start, int end){
-	Random r = new Random();
-	int pivot = r.nextInt(end-start) + start;
-	int v = data[pivot];
-	
-	
-	int i = start+1;
-
-	System.out.println("Pivot: " + pivot + "\n V: " + v + "\n");
-	
-	swap(data, pivot, start);
-        //System.out.println(arr(data));
-
-	while (i<=end){
-	    if (data[i]==v) i++;
-	    else if (data[i]<v){
-		swap(data,start,i);
-		i++;
-		start++;
-	    }
-	    else {
-		swap(data,end,i);
-		end--;
-	    }
+    public static void partition(int []data, int start, int end){
+	if (end - start > 1){
+	    Random r = new Random();
+	    int pivot = r.nextInt(end-start) + start;
+	    int v = data[pivot];
+	    int i = start+1;
+	    
+	    int origin = start;
+	    int finish = end;
+	    
+	    //System.out.println("Pivot: " + pivot + "\n V: " + v + "\n");
+	    swap(data, pivot, start);
 	    //System.out.println(arr(data));
+	    while (i<=end){
+		if (data[i]==v) i++;
+		else if (data[i]<v){
+		    swap(data,start,i);
+		    i++;
+		    start++;
+		}
+		else {
+		    swap(data,end,i);
+		    end--;
+		}
+	    System.out.println(arr(data));
+	    }
+	    
+	    partition(data, origin, start);
+	    partition(data, end, finish);
+	    
 	}
-
-	
-	
-	
 	//System.out.println(arr(data));
-	return (start+end)/2;
     }
     
 
@@ -73,16 +95,28 @@ public class Quick{
 	return ans;
     }
 
+    //QUICKSORT==========================================================================================
+    //QUICKSORT==========================================================================================  
+    //QUICKSORT==========================================================================================  
+
+
+    public static void quicksort(int []data){
+	partition(data, 0, data.length-1);
+	System.out.println(arr(data));
+    } 
 
 
 
+
+
+    //============================================
 
     public static void main (String [] args){
-	int [] ar = new int[20];
+	int [] ar = new int[1000];
 	String s = "";
 	Random r = new Random();
 	for (int i = 0; i < ar.length; i++){
-	    ar[i] = r.nextInt(10);
+	    ar[i] = r.nextInt(3);
 	    s += ar[i]+",";
 	}
 
@@ -94,7 +128,7 @@ public class Quick{
 	//System.out.println(d);
 
 
-	System.out.println(quickselect(ar,6));
+	quicksort(ar);
 	
 
     }
