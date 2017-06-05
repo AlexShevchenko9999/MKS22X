ffbe12f008f664c82040dced0d6cce03d55c3239
Alex Shevchenko <alexshevchenko9999@gmail.com>
2017-06-04 21:34:44 -0400

    Update

--- 15Maze/QueueFrontier.java
+++ 15Maze/QueueFrontier.java
@@ -1,14 +1,26 @@
 import java.util.*;
 public class QueueFrontier implements Frontier {
 
-    private Queue<Location> q = new Queue<Location>();
+    private LinkedList<Location> q;
+    private int size;
+
+    public QueueFrontier(){
+	size = 0;
+	q = new LinkedList<Location>();
+    }
 
     public void add(Location s){
 	q.add(s);
+	size++;
     }
 
     public Location next(){
-	return q.peek();
+	return q.remove();
+	size--;
+    }
+
+    public boolean isEmpty(){
+	return q.peek()==null;
     }
 
 }
